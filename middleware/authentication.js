6e6ea586e0b6247ff18ab17252bcf9b391c17386
Neko1122000 <bonjourneko1122000@gmail.com>
2020-02-07 14:33:19 +0700

    update

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 const config = require('../config/config');
 const User = require('../models/User');
 
-exports.authencate = async (req, res, next) => {
+exports.authenticate = async (req, res, next) => {
     /*if (!req.headers.cookie) return res.status(401).send({ auth: false, message: 'No token provided.' });
     const cookie = cookies.parse(req.headers.cookie);
     if (!cookie.token) return res.status(401).send({ auth: false, message: 'No token provided.' });*/
@@ -11,19 +11,22 @@ exports.authencate = async (req, res, next) => {
     //console.log(auth);
     if (auth == null) return res.status(500).send("Not authorized");
     const token = auth.split(" ");
-    if (token[0] !== 'Bearer') return res.status(500).send(err.message);
+    if (token[0] !== 'Bearer') return res.status(500).send("Not authorized");
     //console.log(token);
-    jwt.verify(token[1], config.secret, function(err, decoded) {
+    jwt.verify(token[1], config.secret, async function(err, decoded) {
         if (err) {
             return res.status(500).send(err.message);
         }
 
+        const user = await User.findById(decoded.id);
+        if (!user) return res.status(200).send("User not found");
         req.userId = decoded.id;
+
         next();
     });
 };
 
-exports.verify = async (req, res, next) => {
+exports.isAdmin = async (req, res, next) => {
     try {
         const user = await User.findById(req.userId);
         if (!user)  return res.status(500).send("Not authorized");
--- routes/router.js
+++ routes/router.js
@@ -6,20 +6,20 @@ const router = express.Router();
 
 router.post('/api/v1/user/register', user.register);                // user.register
 router.post('/api/v1/user/login', user.login);                      // user.login
-router.get('/api/v1/user/me',auth.authencate, user.getSingleUser);                  // user.getSingleUser
-router.get('/api/v1/user/orders',auth.authencate, user.getOrders);                  // user.getOrders
-router.put('/api/v1/user', auth.authencate, user.update);                           // user.update
-router.put('/api/v1/user/change-password',auth.authencate, user.changePassword);    // user.changePassword
+router.get('/api/v1/user/me',auth.authenticate, user.getSingleUser);                  // user.getSingleUser
+router.get('/api/v1/user/orders',auth.authenticate, user.getOrders);                  // user.getOrders
+router.put('/api/v1/user', auth.authenticate, user.update);                           // user.update
+router.put('/api/v1/user/change-password',auth.authenticate, user.changePassword);    // user.changePassword
 
-router.get('/api/v1/users', auth.authencate, auth.verify, user.getUsers);                          // user.getUsers
-router.delete('/api/v1/users/:id', auth.authencate, auth.verify, user.delete);
+router.get('/api/v1/users', auth.authenticate, auth.isAdmin, user.getUsers);                          // user.getUsers
+router.delete('/api/v1/users/:id', auth.authenticate, auth.isAdmin, user.delete);
 
-router.put('/api/v1/products/:id', auth.authencate, auth.verify, product.update);                  // product.update
-router.delete('/api/v1/products/:id', auth.authencate, auth.verify, product.delete); // product.delete
-router.post('/api/v1/products', auth.authencate, auth.verify, product.create);                     // product.create
+router.put('/api/v1/products/:id', auth.authenticate, auth.isAdmin, product.update);                  // product.update
+router.delete('/api/v1/products/:id', auth.authenticate, auth.isAdmin, product.delete); // product.delete
+router.post('/api/v1/products', auth.authenticate, auth.isAdmin, product.create);                     // product.create
 
 router.get('/api/v1/products', product.getProducts);                 // product.getProducts
 router.get('/api/v1/products/:id', product.getSingleProduct);        // product.getSingleProduct
-router.post('/api/v1/products/:id/checkout', auth.authencate, product.checkout);      // product.checkout
+router.post('/api/v1/products/:id/checkout', auth.authenticate, product.checkout);      // product.checkout
 
 module.exports = router;
