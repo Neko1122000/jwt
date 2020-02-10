7036b78b60c40cc3ea4b7b97b16623192d254e6b
Neko1122000 <bonjourneko1122000@gmail.com>
2020-02-10 15:39:46 +0700

    update

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -3,27 +3,27 @@ const config = require('../config/config');
 const User = require('../models/User');
 
 exports.authenticate = async (req, res, next) => {
-    /*if (!req.headers.cookie) return res.status(401).send({ auth: false, message: 'No token provided.' });
-    const cookie = cookies.parse(req.headers.cookie);
-    if (!cookie.token) return res.status(401).send({ auth: false, message: 'No token provided.' });*/
+    try {
+        const auth = req.get('Authorization');
+        if (auth == null) return res.status(500).send("Not authorized");
+        const token = auth.split(" ");
+        if (token[0] !== 'Bearer') return res.status(500).send("Not authorized");
 
-    const auth = req.get('Authorization');
-    //console.log(auth);
-    if (auth == null) return res.status(500).send("Not authorized");
-    const token = auth.split(" ");
-    if (token[0] !== 'Bearer') return res.status(500).send("Not authorized");
-    //console.log(token);
-    jwt.verify(token[1], config.secret, async function(err, decoded) {
-        if (err) {
-            return res.status(500).send(err.message);
-        }
+        jwt.verify(token[1], config.secret, async function(err, decoded) {
+            if (err) {
+                return res.status(500).send(err.message);
+            }
 
-        const user = await User.findById(decoded.id);
-        if (!user) return res.status(200).send("User not found");
-        req.userId = decoded.id;
+            const user = await User.findById(decoded.id);
+            if (!user) return res.status(200).send("User not found");
+            req.userId = decoded.id;
 
-        next();
-    });
+            next();
+        });
+    } catch (e) {
+        const message = e.message;
+        res.status(500).send(message);
+    }
 };
 
 exports.isAdmin = async (req, res, next) => {
